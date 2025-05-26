c1d28e6c71083c5fa430a01cfbb89eecf50fb1d3
StandAlone404 <gennadiy.dev@yandex.ru>
2025-05-26 12:52:06 +0300

    formatAsJson

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,15 +2,22 @@
 
 namespace Differ\Formatters\Json;
 
+use JsonException;
+
 /**
  * Форматирует diff-дерево в JSON формат.
  *
  * @param array $data Данные diff.
  *
  * @return string Отформатированная строка.
+ * @throws JsonException
  */
 
 function formatedAsJson(array $data): string
 {
-    return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    $json = json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+    if ($json === false) {
+        throw new \RuntimeException('Failed to encode JSON');
+    }
+    return $json;
 }
